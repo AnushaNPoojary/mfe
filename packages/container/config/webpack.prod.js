21dc2c8d5ac41264f0c7cde61405335db451547e
anushap1998 <anusha.poojari@goevive.com>
2024-11-01 12:42:18 +0530

    adding invalidation to cloudfront

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig= {
         new ModulefederationPlugin({
             name:"container",
             remotes:{
-                marketing: "marketing@${domain}/marketing/remoteEntry.js"
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
             },
             shared: packagejson.dependencies
         })
