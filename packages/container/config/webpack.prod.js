a2d41856dc2f4ac730d07e4bcad76d23d4345a0e
anushap1998 <anusha.poojari@goevive.com>
2024-11-01 14:56:09 +0530

    adding invalidation to cloudfront

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,27 +1,25 @@
-const {merge}= require('webpack-merge');
-const ModulefederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const packagejson = require('../package.json');
-const commonConfig = require('./webpack.common')
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
-const prodConfig= {
-    mode: "production",
-    output:{
-        filename: '[name].[contenthash].js',
-        publicPath: '/container/latest/'
-    },
-    plugins:[
-        new ModulefederationPlugin({
-            name:"container",
-            remotes:{
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
-            },
-            shared: packagejson.dependencies
-        })
-    ]
-
-}
-
-
-module.exports= merge(commonConfig, prodConfig)
+module.exports = merge(commonConfig, prodConfig);
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import MarketingApp from "../Components/MarketingApp";
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    return (
-        <div>
-            <h1>Hello there!!!!!</h1>
-            <hr />
-            <MarketingApp />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hi there!!!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
