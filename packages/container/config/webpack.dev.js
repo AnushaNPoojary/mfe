8c69aa535d49662154b08d5d9be97ed778c67524
anushap1998 <anusha.poojari@goevive.com>
2024-11-02 16:13:23 +0530

    adding final updates

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -5,13 +5,13 @@ const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
-  output:{
-    publicPath : 'http://localhost:8080/'
+  output: {
+    publicPath: 'http://localhost:8080/',
   },
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: '/index.html',
+      historyApiFallback: true,
     },
   },
   plugins: [
@@ -20,6 +20,7 @@ const devConfig = {
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
         auth: 'auth@http://localhost:8082/remoteEntry.js',
+        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
       },
       shared: packageJson.dependencies,
     }),
